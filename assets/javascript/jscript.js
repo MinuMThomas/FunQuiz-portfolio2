1385d127c50117a55b46ac574dd3874e80db2135
MinuMThomas <minumarytomas@gmail.com>
2022-09-01 15:47:54 +0000

    add javascript

---
+++ assets/javascript/jscript.js
@@ -0,0 +1,7 @@
+//begin the quiz
+const quizBtn = document.getElementById('btn-start');
+//add event listener
+quizBtn.addEventListener('click', quizBegin);
+function quizBegin(){
+    console.log("begin the quiz")
+}
