8f9e27c5c5855a210c7b94ce69d38af8452f9d46
MinuMThomas <minumarytomas@gmail.com>
2022-09-01 12:34:02 +0000

    add javascript to begin the game

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,10 @@
+const quizQuestList =document.getElementById('quiz');
 const quizBtn = document.getElementById('btn-start');
 quizBtn.addEventListener('click', quizBegin);
-
 // method for begin the quiz
 function quizBegin() {
+// console.log("begin the quiz"
+
+quizQuestList.classList.remove('absent')
 
 }
