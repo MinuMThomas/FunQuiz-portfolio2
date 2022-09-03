6970d5c817f338783cc8b1b38285e7b44260fdfd
MinuMThomas <minumarytomas@gmail.com>
2022-09-03 21:10:07 +0000

    add score function and alert to JS

--- assets/javascript/jscript.js
+++ assets/javascript/jscript.js
@@ -1,108 +1,121 @@
-//quiz questions
 const questions = [{
-        question: "What is always coming but never arrives?",
-        correct: "Tomorrow",
-        answers: [
-            "car",
-            "river",
-            "Tomorrow",
-            "bus"
-        ]
-    },
-    {
-        question: "What word is spelled incorrectly in every single dictionary??",
-        correct: "Incorrectly",
-        answers: [
-            "parliament",
-            "Incorrectly",
-            "promise",
-            "thursday"
-        ]
-    },
-    {
-        question: "What never asks a question but gets answered all the time??",
-        correct: "phone",
-        answers: [
-            "phone",
-            "radio",
-            "television",
-            "tree"
-        ]
-    },
-    {
-        question: "What is always coming but never arrives?",
-        correct: "Tomorrow",
-        answers: [
-            "car",
-            "river",
-            "Tomorrow",
-            "bus"
-        ]
-    },
-    {
-        question: "What can one catch that is not thrown??",
-        correct: "cold",
-        answers: [
-            "fish",
-            "leaf",
-            "cold",
-            "water"
-        ]
-    },
-    {
-        question: " What can provide us with power and strength to walk through a wall??",
-        correct: "A door",
-        answers: [
-            "A door",
-            "A wall",
-            "muscles",
-            "magic",
-        ]
-    },
-    {
-        question: "When you drop a white hat in the Black sea, what will it become??",
-        correct: "It becomes wet",
-        answers: [
-            "It becomes light",
-            "It becomes wet",
-            "It becomes white",
-            "It becomes black",
-        ]
-    },
-    {
-        question: "Despite its name, what is actually Bombay Duck?",
-        correct: "type of fish",
-        answers: [
-            "type of turtle",
-            "type of chicken",
-            "type of duck",
-            "type of fish"
-        ]
-    },
-];
+    question: "What is always coming but never arrives?",
+    correct: "Tomorrow",
+    answers: [
+        "car",
+        "river",
+        "Tomorrow",
+        "bus"
+    ]
+},
+{
+    question: "What word is spelled incorrectly in every single dictionary??",
+    correct: "Incorrectly",
+    answers: [
+        "parliament",
+        "Incorrectly",
+        "promise",
+        "thursday"
+    ]
+},
+{
+    question: "What never asks a question but gets answered all the time??",
+    correct: "phone",
+    answers: [
+        "phone",
+        "radio",
+        "television",
+        "tree"
+    ]
+},
+{
+    question: "What is always coming but never arrives?",
+    correct: "Tomorrow",
+    answers: [
+        "car",
+        "river",
+        "Tomorrow",
+        "bus"
+    ]
+},
+{
+    question: "What can one catch that is not thrown??",
+    correct: "cold",
+    answers: [
+        "fish",
+        "leaf",
+        "cold",
+        "water"
+    ]
+},
+{
+    question: " What can provide us with power and strength to walk through a wall??",
+    correct: "A door",
+    answers: [
+        "A door",
+        "A wall",
+        "muscles",
+        "magic",
+    ]
+},
+{
+    question: "When you drop a white hat in the Black sea, what will it become??",
+    correct: "It becomes wet",
+    answers: [
+        "It becomes light",
+        "It becomes wet",
+        "It becomes white",
+        "It becomes black",
+    ]
+},
+{
+    question: "Despite its name, what is actually Bombay Duck?",
+    correct: "type of fish",
+    answers: [
+        "type of turtle",
+        "type of chicken",
+        "type of duck",
+        "type of fish"
+    ]
+}, {
+    question: "Which ywar is this?",
+    correct: "2022",
+    answers: [
+        "2021",
+        "2018",
+        "2078",
+        "2022"
+    ]
+}];
 
 const quizBtn = document.getElementById('btn-starts');
 const nextBtn = document.getElementById('btn-submits');
 const quizQuestList = document.getElementById('quiz');
+const scoreContent = document.getElementById('score');
 
 
-let quizQuestLive, score;
+
+let quizQuestLive
+let score = 0;
+let noOfQuestions = questions.length;
 
 quizBtn.addEventListener('click', quizBegin);
-nextBtn.addEventListener('click', function() {
-    quizQuestLive++; nextQuizQuest();
+nextBtn.addEventListener('click', function () {
+    quizQuestLive++;
+    nextQuizQuest();
+   
 });
 
 function quizBegin() {
     quizBtn.classList.add('absent'); //hide start button after first question
-    quizQuestLive = 0 ;
+    quizQuestLive = 0;
     quizQuestList.classList.remove('absent');
     nextQuizQuest();
-    alert("BEGIN THE QUIZ!!!!")
+    alert("!!!!!BEGIN THE QUIZ!!!!");
 }
 // next question 
 function nextQuizQuest() {
-    defReset();
+    defReset(); //reset to default
     viewQuizQuest(questions[quizQuestLive]);
 }
 //question
@@ -111,16 +124,18 @@ const listOfAnswer = document.getElementById('btn-ans');
 
 function viewQuizQuest(question) {
     questsQuiz.innerText = question.question;
+    //use forEach call back fn to each array element 
     question.answers.forEach(answer => {
-        const listItems = document.createElement('listItems');
+        const listItems = document.createElement('li');
         listItems.innerText = answer;
         listItems.classList.add('ans');
         if (answer == question.correct) {
-            listItems.dataset.correct = answer.correct;
+            listItems.dataset.correct = answer;
         }
         listItems.addEventListener('click', quizAns);
         listOfAnswer.appendChild(listItems);
     });
+   
 }
 //function to reset to default state after each question
 function defReset() {
@@ -132,32 +147,64 @@ function defReset() {
 }
 
 function quizAns(ansC) {
+    // console.log(ansC);
     const ansBtn = ansC.target;
+    // console.log(ansBtn);
     const correct = ansBtn.dataset.correct;
+    // console.log(correct);
 
     checkAnsStatus(document.body, correct);
+    // console.log(listOfAnswer);
     //array to use for each loop - include all questions and restart after last question
     Array.from(listOfAnswer.children).forEach(listItems => {
-        checkAnsStatus(listItems, listItems.dataset.correct);
+        markAnswers(listItems, listItems.dataset.correct);
     });
+    updateScore(score, noOfQuestions);
     if (questions.length > quizQuestLive + 1) {
         nextBtn.classList.remove('absent');
+
     } else {
         quizBtn.innerText = 'Restart';
         quizBtn.classList.remove('absent');
     }
+
 }
 // added background color in css for right and wrong answers
 function checkAnsStatus(element, correct) {
     removeStat(element);
+   
     if (correct) {
         element.classList.add('correct');
+        score++;
+        alert('well done');
+        
+    } else {
+        element.classList.add('wrong');
+        alert('sorry');
+    }
+}
+//color answer background
+function markAnswers(element, correct) {
+    if (correct) {
+        element.classList.add('correct');        
     } else {
         element.classList.add('wrong');
     }
 }
 
 function removeStat(element) {
+
     element.classList.remove('correct');
+
     element.classList.remove('wrong');
+}
+// score add
+const scoreDiv = document.getElementById('valid');
+const total = document.getElementById('totalQuestions');
+function updateScore(score, noOfQuestions) {
+    scoreContent.classList.remove('absent');
+    
+    scoreDiv.innerHTML = score;
+    total.innerHTML = noOfQuestions;
+
 }
