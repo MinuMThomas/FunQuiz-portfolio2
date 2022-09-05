eb2d46ee0c05015f687941d63daa6022a92f3098
MinuMThomas <minumarytomas@gmail.com>
2022-09-05 18:12:37 +0000

    add riddle questions to new js and add script.js in html

---
+++ assets/javascript/main.js
@@ -0,0 +1,103 @@
+const questions = [{
+    question: "What is always coming but never arrives?",
+    correct: "Tomorrow",
+    answers: [
+        "Car",
+        "River",
+        "Tomorrow",
+        "Bus"
+    ]
+},
+{
+    question: "What word is spelled incorrectly in every single dictionary??",
+    correct: "incorrectly",
+    answers: [
+        "parliament",
+        "incorrectly",
+        "promise",
+        "thursday"
+    ]
+},
+{
+    question: "What never asks a question but gets answered all the time??",
+    correct: "phone",
+    answers: [
+        "phone",
+        "radio",
+        "television",
+        "tree"
+    ]
+},
+{
+    question: "What month of the year has 28 days??",
+    correct: "All of them",
+    answers: [
+        "None of them",
+        "February",
+        "All of them",
+        "April"
+    ]
+},
+{
+    question: "What can one catch that is not thrown?",
+    correct: "cold",
+    answers: [
+        "fish",
+        "leaf",
+        "cold",
+        "water"
+    ]
+},
+{
+    question: " What can provide us with power and strength to walk through a wall?",
+    correct: "A door",
+    answers: [
+        "A door",
+        "A wall",
+        "Muscles",
+        "Magic",
+    ]
+},
+{
+    question: "What color is the wind??",
+    correct: "blew",
+    answers: [
+        "red",
+        "white",
+        "blew",
+        "blue"
+    ]
+},
+{
+    question: "Despite its name, what is actually Bombay Duck?",
+    correct: "type of fish",
+    answers: [
+        "type of turtle",
+        "type of chicken",
+        "type of duck",
+        "type of fish"
+    ]
+},
+{
+    question: "Which ywar is this?",
+    correct: "2022",
+    answers: [
+        "2021",
+        "2018",
+        "2022",
+        "2078"
+
+    ]
+},
+{
+    question: "Who is the best?",
+    correct: "Me",
+    answers: [
+        "Ronaldo",
+        "Me",
+        "You",
+        "Messi"
+
+    ]
+},
+];
