95abff1b19e1c2e7552dd45dd75bd71c54cdf1b0
MinuMThomas <minumarytomas@gmail.com>
2022-09-06 12:10:48 +0000

    tidyup the code as deleted commented codes from jscript

--- assets/javascript/jscript.js
+++ assets/javascript/jscript.js
@@ -11,14 +11,13 @@ let quizQuestLive;
 let score = 0;
 let noOfQuestions = questions.length;
 
-quizBtn.addEventListener('click', function(){
-    introduction.style.display='none';
+quizBtn.addEventListener('click', function () {
+    introduction.style.display = 'none';
     quizBegin();
 });
 nextBtn.addEventListener('click', function () {
     quizQuestLive++;
     nextQuizQuest();
-   
 });
 
 function quizBegin() {
@@ -26,7 +25,6 @@ function quizBegin() {
     quizQuestLive = 0;
     quizQuestList.classList.remove('absent');
     nextQuizQuest();
-    // alert("!!!!!RIDDLES HERE!!!!");
     resetScore();
 }
 // next question 
@@ -34,9 +32,7 @@ function nextQuizQuest() {
     defReset(); //reset to default
     viewQuizQuest(questions[quizQuestLive]);
 }
-//question
-// const questsQuiz = document.getElementById('question');
-// const listOfAnswer = document.getElementById('btn-ans');
+//question function
 
 function viewQuizQuest(question) {
     questsQuiz.innerText = question.question;
@@ -51,29 +47,25 @@ function viewQuizQuest(question) {
         listItems.addEventListener('click', quizAns);
         listOfAnswer.appendChild(listItems);
     });
-
 }
 //function to reset to default state after each question
 function defReset() {
     removeStat(document.body);
     nextBtn.classList.add('absent');
-//hide html answer option list
+    //hide html answer option list
     while (listOfAnswer.firstChild) {
         listOfAnswer.removeChild(listOfAnswer.firstChild);
-
     }
 }
 
 function quizAns(ansC) {
-    // console.log(ansC);
+
     const ansBtn = ansC.target;
-    // console.log(ansBtn);
     const correct = ansBtn.dataset.correct;
-    // console.log(correct);
 
     //answer status and move on to next question or restart
     checkAnsStatus(document.body, correct);
-    // console.log(listOfAnswer);
+
     //array to use foreach loop - include all questions and restart after last question
     Array.from(listOfAnswer.children).forEach(listItems => {
         markAnswers(listItems, listItems.dataset.correct);
@@ -81,28 +73,20 @@ function quizAns(ansC) {
     updateScore(score, noOfQuestions);
     if (questions.length > quizQuestLive + 1) {
         nextBtn.classList.remove('absent');
-       
+
     } else {
         quizBtn.innerText = 'TryAgain!';
         quizBtn.classList.remove('absent');
         updateScore(score, noOfQuestions);
-        // tryAgain();
-        // resetScore();
-        // quizBtn.addEventListener('click', resetScore());
     }
-
 }
 // added alert and  increase score
 function checkAnsStatus(element, correct) {
-    // removeStat(element);
     if (correct) {
         element.classList.add('correct');
         score++;
-        // alert('well done');
     } else {
         element.classList.add('wrong');
-        // alert('sorry');
-
     }
 }
 //color answer background
@@ -121,13 +105,10 @@ function removeStat(element) {
     element.classList.remove('wrong');
 }
 // add score 
-
 function updateScore(score, noOfQuestions) {
-
     scoreContent.classList.remove('absent');
     scoreDiv.innerHTML = score;
     total.innerHTML = noOfQuestions;
-
 }
 //reset score back to zero at the end
 function resetScore() {
@@ -136,14 +117,3 @@ function resetScore() {
     scoreDiv.innerHTML = 0;
     total.innerHTML = noOfQuestions;
 }
-//restart riddle again
-// function resetRiddle() {
-//     quizBegin();
-//     scoreContent.classList.remove('absent');
-// }
-
-// function tryAgain() {
-//     resetScore();
-//     resetRiddle();
-
-// }
